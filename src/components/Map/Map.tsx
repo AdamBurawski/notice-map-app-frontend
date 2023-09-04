feb2c371ff5b0ad2b6d7ec4b6a2c2f1d5b9cff78
AdamBurawski <a.burawski.post@gmail.com>
2023-09-04 14:18:37 +0200

    added map from leaflet; changed header position style; added markers to the map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "../../utils/fix-map-icon";
 
 export const Map = () => {
-  return <div className="map"></div>;
+  return (
+    <div className="map">
+      <MapContainer center={[50.2657152, 18.9945008]} zoom={13}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href='https:/>/www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+        />
+        <Marker position={[50.2657152, 18.9945008]}>
+          <Popup>
+            <h2>ADream</h2>
+            <p>agencja interaktywna</p>
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
 };
