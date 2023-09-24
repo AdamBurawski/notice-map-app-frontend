711f645040606472a8da425482a931ac41fdccba
AdamBurawski <a.burawski.post@gmail.com>
2023-09-24 02:34:46 +0200

    Added all endpoints

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "../../utils/fix-map-icon";
+import { SearchContext } from "../../context/search.context";
+import styled from "styled-components";
+
+const SearchFor = styled.h2`
+  position: relative;
+  bottom: -80px;
+  z-index: 1000000;
+  color: white;
+  padding: 5px 5px 40px;
+`;
 
 export const Map = () => {
+  const { search } = useContext(SearchContext);
+
+  useEffect(() => {
+    console.log("Make reqest to search for", search);
+  }, [search]);
+
   return (
     <div className="map">
+      <SearchFor>Search for: {search}</SearchFor>
       <MapContainer center={[50.2657152, 18.9945008]} zoom={13}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { SyntheticEvent, useContext, useState } from "react";
 import { Btn } from "../common/Btn";
+import { SearchContext } from "../../context/search.context";
 
 export const Header = () => {
+  const { search, setSearch } = useContext(SearchContext);
+  const [inputVal, setInputVal] = useState(search);
+
+  const setSearchFromLocalState = (e: SyntheticEvent) => {
+    e.preventDefault();
+    setSearch(inputVal);
+  };
+
   return (
     <header>
       <h1>
         <strong>Mega</strong> Ogłoszenia
       </h1>
       <Btn text="Dodaj ogłoszenie" />
-      <div className="search">
-        <input type="text" /> <Btn text="🔎" />
-      </div>
+      <form className="search" onSubmit={setSearchFromLocalState}>
+        <input
+          type="text"
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+        />{" "}
+        <Btn text="🔎" />
+      </form>
     </header>
   );
 };
